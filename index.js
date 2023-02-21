f7d5f4c6064d207fda33b43051826ffb8203c998
suraj sharma <suryakantsharma.me@gmail.com>
2023-02-21 22:35:37 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  console.log(req?.header);
+  res.send("Welcome to safe-block");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+
+// app.use("/otp", require("./OTP/mail"));
+// app.use("/create",  require("./auth/createUser"));
+// app.use("/tx",  require("./alchemy/transactions"));
+// app.use("/nft",  require("./alchemy/nft"));
+// app.use("/token",  require("./alchemy/token"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
